34d3e43895224962c9c0f3531cedaa3f009def5e
dm <dmotyka777@gmail.com>
2023-11-16 00:43:14 +0100

    theme setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'recruitment-task';
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+
+    })
+  }
+
 }
